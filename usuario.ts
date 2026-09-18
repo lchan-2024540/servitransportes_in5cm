8735cbb455b1bd88650807f2373e5ea58d356218
lchan-2024540 <lchan-2024540@kinal.edu.gt>
2026-09-17 23:59:19 -0600

    fix: se movieron los archivos a la carpeta

--- usuario.ts
+++
@@ -1,9 +0,0 @@
-export interface Usuario {
-    id: number;
-    nombre: string;
-    apellido: string;
-    email: string;
-    telefono?: string;
-    password_hash: string;
-    fecha_registro?: string;
-}
